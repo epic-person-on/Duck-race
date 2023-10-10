ae131924f095c6c56260b5a1d968052f23f354aa
epic-person-on <123142178+epic-person-on@users.noreply.github.com>
2023-10-10 13:38:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+// firebase-config.js
+// Initialize Firebase with your configuration settings
+const firebaseConfig = {
+    apiKey: 'AIzaSyArg0NFirv13y0uJaosaqfsMt8Su_inmsQ',
+    authDomain: 'duck-a701a.firebaseapp.com',
+    projectId: 'duck-a701a',
+    storageBucket: 'gs://duck-a701a.appspot.com',
+    messagingSenderId: '365384333064',
+    appId: '1:365384333064:web:2329438a5333f4b2f41ebd',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Authenticate the user (Google Sign-In) if they are not already signed in.
+firebase.auth().onAuthStateChanged(user => {
+    if (!user) {
+        // User is not signed in, redirect to the login page.
+        window.location.href = 'login.html'; // Create a login page to handle authentication.
+    }
+});
 // index.js
 // Generate a unique random number for the user.
 const uniqueRandomNumber = uuidv4();
